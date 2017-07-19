36265fa4d40dd0da409a5aae60605df13df16637
Emily Huang <emily092793@gmail.com>
2017-07-20 00:01:53 +0800

    first try NY search article API

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ function loadData() {
     var $nytElem = $('#nytimes-articles');
     var $greeting = $('#greeting');
     var googleStreetViewImageAPIKEY = 'AIzaSyB5Zusczf03RjQryWwOlHxboT0FKawmB7k';
+    var NYSearchArticleAPIKEY = '376f99e151a14628be472f37e34a6956';
     // clear out old data before new request
     $wikiElem.text("");
     $nytElem.text("");
@@ -13,15 +14,32 @@ function loadData() {
     // load streetview
     var streetStr = $('#street').val();
     var cityStr = $('#city').val();
+    var address = streetStr + ', ' + cityStr;
+    $greeting.text('So, you want to live at ' + address + '?');
     // YOUR CODE GOES HERE!
     var url = 'https://maps.googleapis.com/maps/api/streetview?size=600x400&';
-    var locationFilter = 'location=' + streetStr + ', ' + cityStr;
+    var locationFilter = 'location=' + address;
     url = url + locationFilter;
     // url = url + '&key=' + googleStreetViewImageAPIKEY;
     var newImg = $(document.createElement('img'))
         .attr({ src: url })
         .addClass("bgimg")
     $body.append(newImg);
+
+    // Your NYTimes AJAX request goes HERE
+    $.getJSON();
+    var NYSearchArticleURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    NYSearchArticleURL += '?' + $.param({
+        'api-key': "376f99e151a14628be472f37e34a6956"
+    });
+    $.ajax({
+        url: NYSearchArticleURL,
+        method: 'GET',
+    }).done(function(result) {
+        console.log(result);
+    }).fail(function(err) {
+        throw err;
+    });
 
     return false;
 };
