88ad9df347fdac9d77991e4480257a17f77fc063
Emily Huang <emily092793@gmail.com>
2017-07-19 23:01:20 +0800

    Loading Google Streetview

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 function loadData() {
 
     var $body = $('body');
@@ -6,14 +5,23 @@ function loadData() {
     var $nytHeaderElem = $('#nytimes-header');
     var $nytElem = $('#nytimes-articles');
     var $greeting = $('#greeting');
-
+    var googleStreetViewImageAPIKEY = 'AIzaSyB5Zusczf03RjQryWwOlHxboT0FKawmB7k';
     // clear out old data before new request
     $wikiElem.text("");
     $nytElem.text("");
 
     // load streetview
-
+    var streetStr = $('#street').val();
+    var cityStr = $('#city').val();
     // YOUR CODE GOES HERE!
+    var url = 'https://maps.googleapis.com/maps/api/streetview?size=600x400&';
+    var locationFilter = 'location=' + streetStr + ', ' + cityStr;
+    url = url + locationFilter;
+    // url = url + '&key=' + googleStreetViewImageAPIKEY;
+    var newImg = $(document.createElement('img'))
+        .attr({ src: url })
+        .addClass("bgimg")
+    $body.append(newImg);
 
     return false;
 };
